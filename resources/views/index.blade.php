9f06f952010f96dd31f564e338d461b986df5022
Christophe Limpalair <christf24@gmail.com>
2015-05-26 22:08:32 -0400

    change text for SYC article on CI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,7 +40,7 @@
             <body>
                 <div class="container">
                     <div class="content">
-                        <div class="title">Lumen with Codeship and CircleCI.</div>
+                        <div class="title">Hello ScaleYourCode.</div>
                     </div>
                 </div>
             </body>
